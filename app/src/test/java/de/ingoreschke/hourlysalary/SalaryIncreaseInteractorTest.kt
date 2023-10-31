137db57c056911086fb3cf97eb911b34ebdaa84f
Ingo Reschke <kontakt@ingo-reschke.de>
2023-10-31 15:08:37 +0100

    add some tests to cover all edges

--- app/src/test/java/de/ingoreschke/hourlysalary/SalaryIncreaseInteractorTest.kt
+++ app/src/test/java/de/ingoreschke/hourlysalary/SalaryIncreaseInteractorTest.kt
@@ -15,4 +15,31 @@ class SalaryIncreaseInteractorTest {
         val actual = salaryIncreaseInteractor.calcIncreasedSalary(startSalary, increasePercentage)
         assertEquals(expected, actual, 0.0)
     }
+
+    @Test
+    fun calcIncreasedSalary_salary_is_0() {
+        val startSalary = 0.0
+        val increasePercentage = 10.0
+        val expected = 0.0
+        val actual = salaryIncreaseInteractor.calcIncreasedSalary(startSalary, increasePercentage)
+        assertEquals(expected, actual, 0.0)
+    }
+
+    @Test
+    fun calcIncreasedSalary_increase_is_0() {
+        val startSalary = 100.0
+        val increasePercentage = 0.0
+        val expected = 100.0
+        val actual = salaryIncreaseInteractor.calcIncreasedSalary(startSalary, increasePercentage)
+        assertEquals(expected, actual, 0.0)
+    }
+
+    @Test
+    fun calcIncreasedSalary_increase_is_negativ() {
+        val startSalary = 100.0
+        val increasePercentage = -20.0
+        val expected = 80.0
+        val actual = salaryIncreaseInteractor.calcIncreasedSalary(startSalary, increasePercentage)
+        assertEquals(expected, actual, 0.0)
+    }
 }
